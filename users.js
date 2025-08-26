34a47013f730134f37d3b1ed32308a44194df15b
Kornkiti <thamjai1999@gmail.com>
2025-08-26 20:55:46 +0700

    Update and rename follow.js to users.js

--- users.js
+++ users.js
@@ -1,4 +1,15 @@
-// follow.js
+// notFollow
+const notFollow = [{
+    'name': 'MODEL GAYDAR',
+    'username': '@model_gaydar',
+    'image': 'https://pbs.twimg.com/profile_images/1740772476609765376/5VTRSQ_d_400x400.jpg'
+},{
+    'name': '彩虹獨角獸',
+    'username': '@aone0912ken_men',
+    'image': 'https://pbs.twimg.com/profile_images/1177994259833614336/A8CKd1Cm_400x400.jpg'
+}];
+
+// follow
 const follow = [{
     'name': 'ゆー',
     'username': '@bro_yu_new',
